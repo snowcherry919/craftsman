5a7042651231b5b10888147c01eb50b94906f853
dada <dada94fight@gmail.com>
2020-06-14 18:17:10 +0800

    Initial commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('.checkoutCardHeader').click(function(){
+        // 價格開啟與隱藏
+        $(".totalPrice").toggle();
+        //箭頭旋轉
+        $(".rotate").toggleClass('active');
+    })
+    
+});
+
+
+ 
