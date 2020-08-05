9dd0a4d91c0a6fc0c7fdc777b30554c87eb37403
snowcherry <snowcherry919@gmail.com>
2020-08-05 23:56:17 +0800

    縮排

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,14 +1,9 @@
-
-
 $(document).ready(() => {
-    $('.checkoutCardHeader').click(function(){
+    $('.checkoutCardHeader').click(function () {
         // 價格開啟與隱藏
         $(".totalPrice").toggle();
         //箭頭旋轉
         $(".rotate").toggleClass('active');
     })
-    
+
 });
-
-
- 
