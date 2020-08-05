72944bfa4017d844c2cdbe007b61888eb40ca6d4
snowcherry <snowcherry919@gmail.com>
2020-08-05 10:34:04 +0800

    Update 2020-08-05T02:33:52.183Z

---
+++ assets/js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.checkoutCardHeader').click(function () {
+    // 價格開啟與隱藏
+    $(".totalPrice").toggle(); //箭頭旋轉
+
+    $(".rotate").toggleClass('active');
+  });
+});
+//# sourceMappingURL=all.js.map
